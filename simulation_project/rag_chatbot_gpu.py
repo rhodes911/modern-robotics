d9cceec45f102f25dda3b05cbd8f9e9ca2942c67
rhodes911 <rhodes911@gmail.com>
2025-11-20 19:42:11 +0000

    Fix RetrievalQA import path

--- simulation_project/rag_chatbot_gpu.py
+++ simulation_project/rag_chatbot_gpu.py
@@ -10,7 +10,7 @@ import chromadb
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.llms import Ollama
-from langchain.chains import RetrievalQA
+from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 import torch
